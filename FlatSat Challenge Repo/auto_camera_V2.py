ee6e18771afb0e9f01f84ff563e04a6290376dfe
Ethanwashere1 <140445400+Ethanwashere1@users.noreply.github.com>
2025-07-04 19:51:18 +0000

    modified:   FlatSat Challenge Repo/auto_camera_V2.py Debug attempt 5

--- FlatSat Challenge Repo/auto_camera_V2.py
+++ FlatSat Challenge Repo/auto_camera_V2.py
@@ -48,10 +48,11 @@ def capture(dir ='roll', target_angle = 30):
         #TODO: Everything else! Be sure to not take a picture on exactly a
         #certain angle: give yourself some margin for error.
         while not(target_angle + 5 >= prev_angle[0] and target_angle - 5 <= prev_angle[0]):
-            if prev_angle > target_angle + 5:
-                print("Move the CubeSat by at least " + (prev_angle - (target_angle + 5)) + " degrees to the left.")
+            if prev_angle[0] > target_angle + 5:
+                print("Move the CubeSat by at least " + (prev_angle[0] - (target_angle + 5)) + " degrees to the left.")
             else:
-                print("Move the CubeSat by at least " + ((target_angle + 5) - prev_angle) + " degrees to the right.")
+                print("Move the CubeSat by at least " + ((target_angle + 5) - prev_angle[0]) + " degrees to the right.")
+            prev_angle = set_initial(offset_mag)
         print("Hold position")
         take_photo()
         
